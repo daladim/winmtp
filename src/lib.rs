e00fda0ecd1378af7215f7efa0efa57b1582fb12
daladim <daladim@users.noreply.github.com>
2023-03-04 23:41:28 +0100

    [doc]

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,19 @@
 //! Microsoft provides a [COM API for WPD (Windows Portable Devices)](https://learn.microsoft.com/en-us/windows/win32/wpd_sdk/programming-guide).<br/>
 //! It also provides [raw bindings over these COM APIs](https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/Devices/PortableDevices/index.html).
 //!
-//! This create provides a safe Rust abstraction over this API.
+//! This create provides a safe Rust abstraction over this API.<br/>
+//!
+//! # What is MTP
+//!
+//! MTP stands for "Media Transfer Protocol". This is how many devices expose themselves when plugged by USB.<br/>
+//! Most (every?) Android device supports it.
 //!
 //! # Important note
 //!
 //! The WPD API is fairly large. This crate only provides a small fraction of what is possible.<br/>
-//! Basically, only device enumeration, simple content enumeration, and content transfer are implemented.<br/>
+//! Basically, only device enumeration, simple content enumeration, and content transfer from and to devices that expose disk-like contents are implemented.
+//!
+//! Much of this crate implements features from code samples at <https://github.com/microsoft/Windows-classic-samples/blob/HEAD/Samples/PortableDeviceCOM/>.<br/>
 //! Much can be added in the future. Contributions are welcome!
 //!
 //! # Usage
