1a613d59a71e122e675853762ffb03625b8f5619
daladim <daladim@users.noreply.github.com>
2023-03-10 11:58:33 +0100

    Added ObjectType::as_guid()

--- src/object/object_type.rs
+++ src/object/object_type.rs
@@ -7,7 +7,7 @@ use windows::Win32::Devices::PortableDevices::{
     WPD_CONTENT_TYPE_MEMO, WPD_CONTENT_TYPE_MIXED_CONTENT_ALBUM, WPD_CONTENT_TYPE_NETWORK_ASSOCIATION,
     WPD_CONTENT_TYPE_PLAYLIST, WPD_CONTENT_TYPE_PROGRAM, WPD_CONTENT_TYPE_SECTION, WPD_CONTENT_TYPE_TASK,
     WPD_CONTENT_TYPE_TELEVISION, WPD_CONTENT_TYPE_UNSPECIFIED, WPD_CONTENT_TYPE_VIDEO, WPD_CONTENT_TYPE_VIDEO_ALBUM,
-    WPD_CONTENT_TYPE_WIRELESS_PROFILE
+    WPD_CONTENT_TYPE_WIRELESS_PROFILE, WPD_CONTENT_TYPE_ALL,
 };
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -76,6 +76,41 @@ impl ObjectType {
             WPD_CONTENT_TYPE_VIDEO_ALBUM            => Self::VideoAlbum,
             WPD_CONTENT_TYPE_WIRELESS_PROFILE       => Self::WirelessProfile,
             _                                       => Self::Unknown,
+        }
+    }
+
+    pub fn as_guid(&self) -> GUID {
+        match self {
+            Self::Appointment        => WPD_CONTENT_TYPE_APPOINTMENT,
+            Self::Audio              => WPD_CONTENT_TYPE_AUDIO,
+            Self::AudioAlbum         => WPD_CONTENT_TYPE_AUDIO_ALBUM,
+            Self::Calendar           => WPD_CONTENT_TYPE_CALENDAR,
+            Self::Certificate        => WPD_CONTENT_TYPE_CERTIFICATE,
+            Self::Contact            => WPD_CONTENT_TYPE_CONTACT,
+            Self::ContactGroup       => WPD_CONTENT_TYPE_CONTACT_GROUP,
+            Self::Document           => WPD_CONTENT_TYPE_DOCUMENT,
+            Self::Email              => WPD_CONTENT_TYPE_EMAIL,
+            Self::Folder             => WPD_CONTENT_TYPE_FOLDER,
+            Self::FunctionalObject   => WPD_CONTENT_TYPE_FUNCTIONAL_OBJECT,
+            Self::GenericFile        => WPD_CONTENT_TYPE_GENERIC_FILE,
+            Self::GenericMessage     => WPD_CONTENT_TYPE_GENERIC_MESSAGE,
+            Self::Image              => WPD_CONTENT_TYPE_IMAGE,
+            Self::ImageAlbum         => WPD_CONTENT_TYPE_IMAGE_ALBUM,
+            Self::MediaCast          => WPD_CONTENT_TYPE_MEDIA_CAST,
+            Self::Memo               => WPD_CONTENT_TYPE_MEMO,
+            Self::MixedContentAlbum  => WPD_CONTENT_TYPE_MIXED_CONTENT_ALBUM,
+            Self::NetworkAssociation => WPD_CONTENT_TYPE_NETWORK_ASSOCIATION,
+            Self::Playlist           => WPD_CONTENT_TYPE_PLAYLIST,
+            Self::Program            => WPD_CONTENT_TYPE_PROGRAM,
+            Self::Section            => WPD_CONTENT_TYPE_SECTION,
+            Self::Task               => WPD_CONTENT_TYPE_TASK,
+            Self::Television         => WPD_CONTENT_TYPE_TELEVISION,
+            Self::Unspecified        => WPD_CONTENT_TYPE_UNSPECIFIED,
+            Self::Video              => WPD_CONTENT_TYPE_VIDEO,
+            Self::VideoAlbum         => WPD_CONTENT_TYPE_VIDEO_ALBUM,
+            Self::WirelessProfile    => WPD_CONTENT_TYPE_WIRELESS_PROFILE,
+            Self::All                => WPD_CONTENT_TYPE_ALL,    // Not sure about this one...
+            Self::Unknown            => WPD_CONTENT_TYPE_ALL,    // Not sure about this one...
         }
     }
 }
