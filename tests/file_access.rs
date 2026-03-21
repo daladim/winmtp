405a2275e165fef9e1976e66d65c7e15571d5ae8
jannikac <jannikac@mailbox.org>
2026-03-21 16:51:58 +0100

    add tests for create_write_stream

--- tests/file_access.rs
+++ tests/file_access.rs
@@ -1,6 +1,7 @@
 //! These test should succeed when an Android device is connected.
 
 use std::ffi::OsStr;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 
 use widestring::U16CString;
@@ -73,6 +74,9 @@ impl DeviceKind {
         // r"Internal shared storage\Download\winmtp_test\Rough Draft (open source mp3 from audiohub.com).mp3"
         PathBuf::from(format!(r"{}\{}\winmtp_test\Rough Draft (open source mp3 from audiohub.com).mp3", self.storage_root_name(), self.downloads_dir_name()))
     }
+    fn write_stream_file_path(&self) -> PathBuf {
+        PathBuf::from(format!(r"{}\{}\winmtp_test_stream\Rough Draft (open source mp3 from audiohub.com).mp3", self.storage_root_name(), self.downloads_dir_name()))
+    }
 }
 
 fn get_device_kind(basic_device: &BasicDevice) -> DeviceKind {
@@ -97,6 +101,8 @@ fn file_access() {
     access_by_id(first_device, &device_kind);
     push_content(first_device, &device_kind);
     pull_content(first_device, &device_kind);
+    writes_file_via_create_write_stream(first_device, &device_kind);
+    verifies_file_written_via_create_write_stream(first_device, &device_kind);
 }
 
 fn access_by_path(basic_device: &BasicDevice, device_kind: &DeviceKind) {
@@ -179,6 +185,34 @@ fn push_content(basic_device: &BasicDevice, device_kind: &DeviceKind) {
     test_folder.push_file(Path::new(EXAMPLE_FILE), true).unwrap();
 }
 
+fn writes_file_via_create_write_stream(basic_device: &BasicDevice, device_kind: &DeviceKind) {
+    let app_identifiers = winmtp::make_current_app_identifiers!();
+    let device = basic_device.open(&app_identifiers, true).unwrap();
+    let content = device.content().unwrap();
+    let download_folder = content.root().unwrap().object_by_path(&device_kind.downloads_dir_path()).unwrap();
+    let test_folder_id = match download_folder.create_subfolder(OsStr::new("winmtp_test_stream")) {
+        Ok(id) => id,
+        Err(winmtp::error::CreateFolderError::AlreadyExists) => {
+            let mut existing_folder = download_folder.object_by_path(Path::new("winmtp_test_stream")).unwrap();
+            existing_folder.delete(true).unwrap();
+            // and try again
+            download_folder.create_subfolder(OsStr::new("winmtp_test_stream")).unwrap()
+        }
+        Err(err) => panic!("{}", err),
+    };
+
+    let test_folder = content.object_by_id(test_folder_id).unwrap();
+    let file_size = std::fs::metadata(Path::new(EXAMPLE_FILE)).unwrap().len();
+    let file_name = Path::new(EXAMPLE_FILE).file_name().unwrap();
+    let mut source_file = std::fs::File::open(Path::new(EXAMPLE_FILE)).unwrap();
+    
+    let mut output_stream = test_folder
+        .create_write_stream(file_name, file_size)
+        .unwrap();
+    std::io::copy(&mut source_file, &mut output_stream).unwrap();
+    output_stream.flush().unwrap();
+}
+
 fn pull_content(basic_device: &BasicDevice, device_kind: &DeviceKind) {
     let app_identifiers = winmtp::make_current_app_identifiers!();
     let device = basic_device.open(&app_identifiers, true).unwrap();
@@ -193,6 +227,23 @@ fn pull_content(basic_device: &BasicDevice, device_kind: &DeviceKind) {
     // Download the file
     let mut input_stream = object.open_read_stream().unwrap();
     let mut output_file = std::fs::File::create(r"tests\assets\pulled-from-device.dat").unwrap();
+    std::io::copy(&mut input_stream, &mut output_file).unwrap();
+}
+
+fn verifies_file_written_via_create_write_stream(basic_device: &BasicDevice, device_kind: &DeviceKind) {
+    let app_identifiers = winmtp::make_current_app_identifiers!();
+    let device = basic_device.open(&app_identifiers, true).unwrap();
+    let object = device.content().unwrap().root().unwrap().object_by_path(&device_kind.write_stream_file_path()).unwrap();
+
+    // Check the file size
+    let metadata = object.properties(&[WPD_OBJECT_SIZE]).unwrap();
+    let retrieved_size = metadata.get_u32(&WPD_OBJECT_SIZE).unwrap();
+    let original_size = std::fs::metadata(Path::new(EXAMPLE_FILE)).unwrap().len();
+    assert_eq!(retrieved_size as u64, original_size);
+
+    // Download the file
+    let mut input_stream = object.open_read_stream().unwrap();
+    let mut output_file = std::fs::File::create(r"tests\assets\created-via-write-stream.dat").unwrap();
     std::io::copy(&mut input_stream, &mut output_file).unwrap();
 }
 
